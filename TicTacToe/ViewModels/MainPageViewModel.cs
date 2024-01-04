de44776e412819decb0922918822e06c3d5e46f1
s1091974@O365st.pu.edu.tw <s1091974@O365st.pu.edu.tw>
2024-01-05 00:51:48 +0800

    加入專案檔案。

---
+++ TicTacToe/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TicTacToe.Views;
+
+namespace TicTacToe.ViewModels
+{
+    /// <summary>
+    /// This is the main page or start page
+    /// </summary>
+    public partial class MainPageViewModel : ObservableObject
+    {
+
+        #region Auto Properties from Fields
+
+        /// <summary>
+        /// Indicates if the computer starts
+        /// </summary>
+        [ObservableProperty]
+        private bool _twoPlayer;
+
+        /// <summary>
+        /// Indicates if the game has two players; otherwise, one player.
+        /// </summary>
+        [ObservableProperty]
+        private bool _computerStarts;
+
+        #endregion Fields
+        
+        #region Auto Relay Commands from Methods
+
+        /// <summary>
+        /// Navigate to the about view
+        /// </summary>
+        [RelayCommand]
+        
+
+        /// <summary>
+        /// Navigate to the game play view
+        /// </summary>
+        
+        public async void StartClick()
+        {
+            try
+            {
+                await Shell.Current.GoToAsync($"{nameof(GamePlayView)}?TwoPlayer={TwoPlayer}&ComputerStarts={ComputerStarts}");
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Unable to resolve type {nameof(GamePlayView)}");
+            }
+        }
+
+        #endregion Relay Commands
+
+
+
+    }
+}
